0743495931a33017c9b5a43fe7b5a1616cbde7a7
hcagricakir <hcagricakir@gmail.com>
2021-12-05 12:41:52 +0300

    likedList kullanimi

--- src/o18CollectionFramework/list_interface/LinkedListKullanimi.java
+++ src/o18CollectionFramework/list_interface/LinkedListKullanimi.java
@@ -3,6 +3,7 @@ package o18CollectionFramework.list_interface;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 // eleman eklemek ve çıkarmak daha hızlı
 // fakat listeyi gezmek daha verimsiz
 
@@ -21,28 +22,49 @@ public class LinkedListKullanimi {
         //      System.out.println(iter.next());
         //  }
         LinkedList<Integer> sayilar1 = new LinkedList<>();
-
-
         ArrayList<Integer> sayilar2 = new ArrayList<>();
 
         linklistiHazirla(sayilar1);
-        for (int i=0; i<90000000;i++){
-            sayilar1.add((int)(Math.random()*5000000));
-        }
-        for (int i=0; i<90000000;i++){
-            sayilar2.add((int)(Math.random()*5000000));
-        }
+        System.out.println(sayilar1);
+        arraylistiHazilra(sayilar2);
+        System.out.println(sayilar2);
+
 
     }
 
-        public static void linklistiHazirla(LinkedList<Integer> sayilar1){
-
+    private static void arraylistiHazilra(ArrayList<Integer> sayilar2) {
+        for (int i = 0; i < 9; i++) {
+            int yeniEklenecekEleman = (int) (Math.random() * 5);
+            siraliEkle(sayilar2, yeniEklenecekEleman);
         }
+    }
 
-        public static void siraliEkle(List<Integer> liste){
-
+    public static void linklistiHazirla(LinkedList<Integer> sayilar1) {
+        for (int i = 0; i < 9; i++) {
+            int yeniEklenecekEleman = (int) (Math.random() * 5);
+            siraliEkle(sayilar1, yeniEklenecekEleman);
         }
+    }
 
+    public static boolean siraliEkle(List<Integer> liste, int yeniEklenecekEleman) {
+
+        ListIterator<Integer> iter = liste.listIterator();
+        while (iter.hasNext()) {
+
+            int karsilastirmaSonucu = iter.next().compareTo(yeniEklenecekEleman);
+            if (karsilastirmaSonucu == 0) {
+                iter.add(yeniEklenecekEleman);
+                return true;
+            } else if (karsilastirmaSonucu > 0) {
+                iter.previous();
+                iter.add(yeniEklenecekEleman);
+                return true;
+            } else {
+            }
+        }
+        iter.add(yeniEklenecekEleman);
+        return true;
+    }
 
 
 }
